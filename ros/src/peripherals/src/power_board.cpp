aa6ff313a6f804e8cb83aa54a4b09a11ca1b840b
lymacasm <lyden.m.smith@gmail.com>
2018-06-16 17:40:52 -0700

    Wrote some power board code. Needs to be tested.

--- ros/src/peripherals/src/power_board.cpp
+++ ros/src/peripherals/src/power_board.cpp
@@ -30,27 +30,92 @@ power_board::~power_board() {
 
 std::string power_board::write(const std::string & out, bool ignore_response, std::string eol)
 {
+    // Flush the output, then the input (order matters, to flush any unwanted responses)
+    connection->flushOutput();
+    connection->flushInput();
+
+    // Send command 
     connection->write(out + eol);
     ROS_INFO("%s", out.c_str());
+
+    // Used for commands where response does not matter (enabling, etc)
     if (ignore_response) {
         return "";
     }
+
+    // Used for commands to read data
     return connection->readline(65536ul, eol);
 }
 
 void power_board::get_powerboard_data(powerboardInfo &msg) {
+    // Get data from power board
     std::string currents = this->write("CRA", false);
     std::string voltages = this->write("VTA", false);
     std::string temperature = this->write("TMP", false);
     std::string humidity = this->write("HUM", false);
     std::string water = this->write("WTR", false);
-    std::string pressure_internal = this->write("PIN");
-    std::string pressure_external = this->write("PEX");
+    std::string pressure_internal = this->write("PIN", false);
+    std::string pressure_external = this->write("PEX", false);
 
-    msg.current_all = 50;
-    msg.current_battery_1 = 50;
-    msg.current_battery_2 = 50;
-    msg.current_motors = 50;
+    // Populate message with current data
+    if(currents.length() >= 12) {       
+        msg.current_battery_1 = (currents[0] << 16) | (currents[1] << 8) | (currents[2]);
+        msg.current_battery_2 = (currents[3] << 16) | (currents[4] << 8) | (currents[5]);
+        msg.current_motors = (currents[6] << 16) | (currents[7] << 8) | (currents[8]);
+        msg.current_system = (currents[9] << 16) | (currents[10] << 8) | (currents[11]);
+    }
+    else {      
+        ROS_INFO("Current data is invalid. Data:%s\n", currents.c_str());
+    }
+
+    // Populate message with voltage data
+    if(voltages.length() >= 4) {        
+        msg.voltage_battery_1 = (voltages[0] << 8) | (voltages[1]);
+        msg.voltage_battery_2 = (voltages[2] << 8) | (voltages[3]);
+    }
+    else {      
+        ROS_INFO("Voltage data is invalid. Data:%s\n", voltages.c_str());
+    }
+
+    // Populate message with temperature data
+    if(temperature.length() >= 2) {     
+        msg.temperature = (temperature[0] << 8) | (temperature[1]);
+    }
+    else {      
+        ROS_INFO("Temperature data is invalid. Data:%s\n", temperature.c_str());
+    }
+
+    // Populate message with humidity data
+    if(humidity.length() >= 2) {     
+        msg.humidity = (humidity[0] << 8) | (humidity[1]);
+    }
+    else {      
+        ROS_INFO("Humidity data is invalid. Data:%s\n", humidity.c_str());
+    }
+
+    // Populate message with water sensor data
+    if(water.length() >= 2) {     
+        msg.water_sensor = (water[0] << 8) | (water[1]);
+    }
+    else {      
+        ROS_INFO("Water data is invalid. Data:%s\n", water.c_str());
+    }
+
+    // Populate message with main housing pressure data
+    if(pressure_internal.length() >= 2) {     
+        msg.internal_pressure = (pressure_internal[0] << 8) | (pressure_internal[1]);
+    }
+    else {      
+        ROS_INFO("Internal housing pressure data is invalid. Data:%s\n", pressure_internal.c_str());
+    }
+
+    // Populate message with external water pressure data
+    if(pressure_external.length() >= 2) {     
+        msg.external_pressure = (pressure_external[0] << 8) | (pressure_external[1]);
+    }
+    else {      
+        ROS_INFO("External water pressure data is invalid. Data:%s\n", pressure_external.c_str());
+    }
 } 
 
 int main(int argc, char ** argv)
@@ -69,8 +134,21 @@ int main(int argc, char ** argv)
 
     ROS_INFO("Using Power Board on fd %s\n", srv.response.device_fd.c_str());
 
-    /* Wait for callbacks */
-    ros::spin();
+    ros::Publisher pub = nh.advertise<peripherals::powerboard>("power_board_data", 10);
+
+    // Main loop
+    ros::Rate r(1);
+    power_board device(srv.response.device_fd);
+    while(ros::ok()) {
+        // Publish message to topic 
+        peripherals::powerboard msg;
+        device.get_powerboard_data(msg);
+        pub.publish(msg);
+
+        // End of loop maintenance
+        ros::spinOnce();
+        r.sleep();
+    }
 
     return 0;
 }
