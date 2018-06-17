c603ed1d97653775ea71066b42eb83effa5d38ec
asinha94 <asinha94@gmail.com>
2018-06-16 18:43:34 -0700

    renamed launchfile

--- ros/src/navigation/src/thrustmap.cpp
+++ ros/src/navigation/src/thrustmap.cpp
@@ -1,28 +1,51 @@
 #include <ros/ros.h>
 #include "navigation/nav.h"
+#include "peripherals/motor.h"
+#include "peripherals/motors.h"
 
-/*  The matrix E was precomputed in Matlab/Octave then dumped here. 
-    The method, summarized below, can be found in Chris Carpenter's Report
-    ..
-*/
-
-const float e_matrix[3][7] = {
-        {0.35355, 0.35292, 0.35355, 0.35292, 0.00141, 0.00141, 0.02099},
-        {0.35355, -0.35292, 0.35355, -0.35292, -0.00141, -0.00141, -0.02099},
-        {-0.00000, 0.00199, 0.00000, 0.00199, 0.49554, 0.49554, -0.06646}};
-
-
-void generate_thrust_val(const navigation::nav::ConstPtr &msg)
+class thrust_controller
 {
+public:
+    thrust_controller(std::string node_name);
+    void generate_thrust_val(const navigation::nav::ConstPtr &msg);
+private:
+    ros::NodeHandle nh;
+    ros::ServiceClient motor_forward;
+    ros::ServiceClient motor_reverse;
+    ros::ServiceClient motor_set_all;
+    ros::ServiceClient motor_stop;
+    ros::ServiceClient motors_stop;
+    const float EMatrix[5][5];
+};
 
+thrust_controller::thrust_controller(std::string node_name) :
+    nh(ros::NodeHandle("~")),
+    motor_forward(nh.serviceClient<peripherals::motor>(node_name + "/setMotorForward")),
+    motor_reverse(nh.serviceClient<peripherals::motor>(node_name + "/setMotorReverse")),
+    motor_set_all(nh.serviceClient<peripherals::motor>(node_name + "/setAllMotors")),
+    motor_stop(nh.serviceClient<peripherals::motor>(node_name + "/stopMotors")),
+    motors_stop(nh.serviceClient<peripherals::motor>(node_name + "/stopAllMotors")),
+    EMatrix{
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0},
+        {0,0,0,0,0}
+    } { }
+
+void thrust_controller::generate_thrust_val(const navigation::nav::ConstPtr &msg)
+{
+    ROS_INFO("X: %.2f Y: %.2f Z: %.2f Speed: %d\n"
+    , msg->direction.x, msg->direction.y, msg->direction.z, msg->speed);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "thrustmap");
     ros::NodeHandle nh("~");
-    ros::Subscriber joy = nh.subscribe<navigation::nav>("/nav/navigation", 1, generate_thrust_val);
-    
+    thrust_controller tc("motor_controller");
+    ros::Subscriber joy = nh.subscribe<navigation::nav>
+        ("/nav/navigation", 1, &thrust_controller::generate_thrust_val, &tc);
     ros::spin();
     return 0;
 }
