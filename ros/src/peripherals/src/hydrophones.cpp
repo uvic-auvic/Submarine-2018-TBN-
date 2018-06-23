e072fe9dd70b7679391482f062430b6cc1ed5fc5
lymacasm <lyden.m.smith@gmail.com>
2018-06-23 15:13:11 -0700

    small changes to hydrophones

--- ros/src/peripherals/src/hydrophones.cpp
+++ ros/src/peripherals/src/hydrophones.cpp
@@ -2,9 +2,10 @@
 
 int main(int argc, char** argv)
 {       
-    ros::init("hydrophones");
+    ros::init(argc, argv, "hydrophones");
     ros::NodeHandle nh("~");
 
     std::string device_id;
     nh.getParam("device_id", device_id);
+
 }
