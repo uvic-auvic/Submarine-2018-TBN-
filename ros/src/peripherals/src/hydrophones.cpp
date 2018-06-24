b3ca7e7dd44e876204e0448dde5d67e9a00fe177
lymacasm <lyden.m.smith@gmail.com>
2018-06-23 22:48:19 -0700

    Can properly read incoming data from hydrophones

--- ros/src/peripherals/src/hydrophones.cpp
+++ ros/src/peripherals/src/hydrophones.cpp
@@ -36,6 +36,7 @@ std::string hydrophones::write(const std::string out, bool ignore_response, cons
 {   
     connection->flush();
     connection->write(out + eol);
+    connection->flushOutput();
 
     if(ignore_response) 
     {   
@@ -56,7 +57,11 @@ void hydrophones::acquire_hydro_data(std::vector<uint16_t> hydro_data[NUM_HYDROP
     do
     {  
         // Get the packet header and relevant information
-        connection->read(header, PKT_HEADER_SIZE);
+        uint8_t lines;
+        if((lines = connection->read(header, PKT_HEADER_SIZE)) != PKT_HEADER_SIZE)
+        {       
+            ROS_ERROR("Did not read enough lines! Read only %u lines.", lines);
+        }
         uint32_t crc = (header[3] << 24) | (header[2] << 16) | (header[1] << 8) | header[0];
         packet_count = header[6];
         packet_idx = header[7];
@@ -67,11 +72,13 @@ void hydrophones::acquire_hydro_data(std::vector<uint16_t> hydro_data[NUM_HYDROP
         connection->read(packet_data, packet_size);
 
         // Check to see if there were any communication errors
+        uint32_t host_crc = stm32f4_crc32(packet_data, packet_size);
         if(stm32f4_crc32(packet_data, packet_size) != crc)
         {       
             ROS_ERROR("Communication error. Executing packet resend.");
-            packet_idx--; // Do this just in case this is the last packet (get through the while loop)
+            packet_idx = 0; // Do this just in case this is the last packet (get through the while loop)
             write("RETRY"); // Send the last packet again
+            delete[] packet_data;
             continue;
         }
         
@@ -84,13 +91,17 @@ void hydrophones::acquire_hydro_data(std::vector<uint16_t> hydro_data[NUM_HYDROP
             hydro_data[data_index % NUM_HYDROPHONES].push_back((packet_data[(i*2)+1] << 8) | packet_data[i*2]); 
         }
 
+        delete[] packet_data;
+
     } while(packet_idx < (packet_count - 1));
+
+    ROS_ERROR("DONE");
 
     connection->flush(); // Sent an extra "NEXT"
 }
 
 uint32_t hydrophones::stm32f4_crc32(uint8_t* data, size_t data_len, uint32_t crc)
-{       
+{
     // Make sure data length is a multiple of 4
     if(data_len % 4)
     {   
@@ -100,18 +111,19 @@ uint32_t hydrophones::stm32f4_crc32(uint8_t* data, size_t data_len, uint32_t crc
 
     // Compute the CRC 32 bits at a time
     for(int i = 0; i < (data_len/4); i++)
-    {   
+    {  
         uint32_t uint32_datum = (data[(i*4)+3] << 24) | (data[(i*4)+2] << 16) | (data[(i*4)+1] << 8) | data[(i*4)];
-        crc ^= uint32_datum;
+        //uint32_t uint32_datum = (data[i*4] << 24) | (data[(i*4)+1] << 16) | (data[(i*4)+2] << 8) | data[(i*4)+3];
+        crc = crc ^ uint32_datum;
         for(int j = 0; j < 32; j++)
-        {       
+        {      
             if(crc & 0x80000000)
-            {       
-                crc = (crc << 1) & 0x04C11DB7;
+            {      
+                crc = (crc << 1) ^ 0x04C11DB7;
             }
             else
             {   
-                crc <<= 1;
+                crc = crc << 1;
             }
         }
     }
@@ -127,13 +139,19 @@ int main(int argc, char** argv)
     monitor::GetSerialDevice srv;
     nh.getParam("device_id", srv.request.device_id);
 
-    ros::ServiceClient client = nh.serviceClient<monitor::GetSerialDevice>("/serial_manager/GetDevicePort");
+    /*ros::ServiceClient client = nh.serviceClient<monitor::GetSerialDevice>("/serial_manager/GetDevicePort");
     if(!client.call(srv)) {     
         ROS_INFO("Couldn't get \"%s\" file descriptor. Shutting down", srv.request.device_id.c_str());
         return 1;
     }
 
-    ROS_INFO("Using Hydrophones on fd %s\n", srv.response.device_fd.c_str());
+    ROS_INFO("Using Hydrophones on fd %s\n", srv.response.device_fd.c_str());*/
+
+    //hydrophones device(srv.response.device_fd.c_str());
+    hydrophones device("/dev/ttyS3");
+
+    std::vector<uint16_t> data[4];
+    device.acquire_hydro_data(data);
    
     ros::spin();
 
