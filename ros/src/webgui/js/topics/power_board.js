29a438310cb74b183285c333b3cff0a322bb6aa1
Andy Bates <bates.andy123@gmail.com>
2018-06-29 01:36:23 -0700

    Added more pages to the site

--- ros/src/webgui/js/topics/power_board.js
+++ ros/src/webgui/js/topics/power_board.js
@@ -1,17 +1,17 @@
-var depth_node = new ROSLIB.Topic({
+var power_board = new ROSLIB.Topic({
     ros : ros,
-    name : '/powerboad/power_board_data',
-    messageType : '/peripherals/powerboard'
+    name : '/power_board/power_board_data',
+    messageType : 'peripherals/powerboard'
   });
 
-  depth_node.subscribe(function(message) {
+  power_board.subscribe(function(message) {
     //console.log('Received message on ' + depth_node.name + ': ' + message.temperature);
-    $('#pb_batt_1_volt').text(message.voltage_battery_1);
-    $('#pb_batt_2_volt').text(message.voltage_battery_2);
+    $('#pb_batt_1_volt').text((message.voltage_battery_1)/1000.0);
+    $('#pb_batt_2_volt').text((message.voltage_battery_2)/1000.0);
     $('#pb_motor_curr').text(message.current_motors);
     $('#pb_system_curr').text(message.current_system);
-    $('#pb_temperature').text(message.temperature);
+    $('#pb_temperature').text(Math.round(message.temperature));
     $('#pb_rh').text(message.humidity);
-    $('#pb_internal_pressure').text(message.internal_pressure);
-    $('#pb_external_pressure').text(message.external_pressure);
+    $('#pb_internal_pressure').text(Math.round(message.internal_pressure));
+    $('#pb_external_pressure').text(Math.round(message.external_pressure));
   });
