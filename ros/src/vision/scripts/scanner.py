c09896a4a714715e2a028b6080d58aac2686e547
asinha94 <asinha94@gmail.com>
2018-08-03 00:45:12 -0700

    added new vision code

--- ros/src/vision/scripts/scanner.py
+++ ros/src/vision/scripts/scanner.py
@@ -14,45 +14,6 @@ class object_scanner:
         self.impub = rospy.Publisher(rospy.get_param("~topic_out_name") + "_video", Image, queue_size=1)
         self.disable = False
         self.dims = (550, 400) # width, height
-
-    # threshold is in terms of pixels
-    # img is mutated
-    def remove_short_paths(self, img, threshold=10):
-        cpy = img.copy()
-        height, width = cpy.shape
-        for y, row in enumerate(img):
-            for x, _ in enumerate(row):
-                clear_list = []
-                if self.dfs(cpy, height, width, x, y, clear_list) < threshold:
-                    # Delete the chain from the original
-                    #self.dfs(img, height, width, x, y)
-                    for ox, oy in clear_list:
-                        img[oy][ox] = 0
-        
-
-    # Run a depth first search on the image
-    # by default the function will clobber any values that it runs into
-    def dfs(self, img, height, width, x, y, clear_list):
-        # check if we're out of bounds
-        if x < 0 or y < 0 or x >= width or y >= height:
-            return 0
-
-        count = 0
-        # check if we just broke the chain
-        if img[y][x] == 0:
-            return count
-        else:
-            # set it to zero so we don't run into it again
-            img[y][x] = 0
-            clear_list.append((x,y))
-
-        # Check everything thats to the right, below and diagonally downwards
-        count += self.dfs(img, height, width, x+1, y, clear_list)
-        count += self.dfs(img, height, width, x-1, y+1, clear_list)
-        count += self.dfs(img, height, width, x, y+1, clear_list)
-        count += self.dfs(img, height, width, x+1, y+1, clear_list)
-
-        return count
         
     def detect(self, img):
         if self.disable:
@@ -65,13 +26,13 @@ class object_scanner:
 
         # canny edge detection
         cv_img = cv2.Canny(cv_img, threshold1=75, threshold2=110)
-        mask = np.ones((self.dims[1]+2, self.dims[0]+2), np.uint8)
-        cv2.floodFill(cv_img, mask, (0,0), 255)
 
+        #mask = np.ones((self.dims[1]+2, self.dims[0]+2), np.uint8)
+        #cv2.floodFill(cv_img, mask, (0,0), 255)
         squashed_height = cv2.reduce(cv_img, 0, cv2.REDUCE_SUM, dtype=cv2.CV_32S)[0]
-
+        
         # find susbtrings of non-zero items in list
-        threshold = 2000
+        threshold = 3000
         centers = []
         start = -1
 
@@ -81,7 +42,10 @@ class object_scanner:
                     start = i
             else:
                 if start != -1:
-                    centers.append((sum(squashed_height[start:i]), start, i))
+                    height_sum = sum(squashed_height[start:i])
+                    gradient = height_sum / ((i - start) ** 3)
+                    if gradient > 4000:
+                        centers.append((height_sum, gradient, start, i))
                     start = -1
 
         if len(centers) < 2:
@@ -96,7 +60,7 @@ class object_scanner:
             self.impub.publish(self.bridge.cv2_to_imgmsg(orig_img, "bgr8"))
             return
 
-        x = (centers[0][1] + centers[0][2] + centers[1][1] + centers[1][2]) / 4
+        x = (centers[0][-2] + centers[0][-1] + centers[1][-2] + centers[1][-1]) / 4
         # Draw line on image to show position to move towards
         cv2.line(orig_img, (x, 0), (x, self.dims[1]), 3)
         it_img = self.bridge.cv2_to_imgmsg(orig_img, "bgr8")
@@ -106,7 +70,7 @@ class object_scanner:
         center_x = self.dims[0] / 2
         offset_x = x - center_x
         msg = offset_position()
-        msg.relative_offset_x = 100 * (float(offset_x) / center_x)
+        msg.relative_offset_x = int(100 * (float(offset_x) / center_x))
         self.pub.publish(msg) # return the position as a percentage of the width
         
 if __name__ == '__main__':
